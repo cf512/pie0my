30ab6ddf74338b78edf6b5f6ba8e5416953756be
cf512 <26924089+cf512@users.noreply.github.com>
2022-11-14 23:53:47 -0600

    Homepage queries all pies

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,12 @@
-var db = require("../models");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 module.exports = function(app) {
-    // Load index page
-    app.get("/", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("index", {
-                msg: "Welcome!",
-                examples: dbExamples
-            });
+    app.get("/", async function(req, res) {
+        const dbPies = await prisma.pie.findMany({});
+        res.render("index", {
+            msg: "Welcome!",
+            pieList: dbPies
         });
     });
 
