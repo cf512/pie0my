7d11fe3a162544481148de26fb075c0486bc4e99
cf512 <26924089+cf512@users.noreply.github.com>
2022-11-15 01:42:17 -0600

    Pie refill route and query

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,36 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const pieData = require("../prisma/seed.js");
 
 module.exports = function(app) {
 
     app.get("/feed", async (req, res) => {
         const pies = await prisma.pie.findMany({});
         res.json(pies);
+    });
+
+    app.get("/pies/refill", async (req, res) => {
+        const pies = await prisma.pie.findMany({});
+
+        switch (pies.length) {
+        case 4:
+        case 3:
+        case 2:
+        case 1:
+            res.send(false);
+            break;
+        case 0:
+            console.log("Start seeding ...");
+            for (const p of pieData) {
+                const pie = await prisma.pie.create({
+                    data: p,
+                });
+                console.log(`Created pie with id: ${pie.id}`);
+            }
+            console.log("Seeding finished.");
+            res.send(true);
+            break;
+        }
     });
 
     app.post("/post", async (req, res) => {
