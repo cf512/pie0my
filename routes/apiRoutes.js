86aa38f01907e4f66c71a2e0eb2ba49530c7a8a8
cf512 <26924089+cf512@users.noreply.github.com>
2022-11-16 09:39:13 -0600

    Removing these routes for now, not needed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,26 +30,6 @@ module.exports = function(app) {
         }
     });
 
-    app.post("/post", async (req, res) => {
-        const { name, imageUrl } = req.body;
-        const pie = await prisma.pie.create({
-            data: {
-                name,
-                imageUrl
-            },
-        });
-        res.json(pie);
-    });
-
-    app.put("/update/:id", async (req, res) => {
-        const id = parseInt(req.params.id);
-        const pie = await prisma.pie.update({
-            where: { id: id },
-            data: {imageUrl: req.body.imageUrl},
-        });
-        res.json(pie);
-    });
-
     app.delete("/pie/:id", async (req, res) => {
         const id = parseInt(req.params.id);
         const pie = await prisma.pie.delete({
